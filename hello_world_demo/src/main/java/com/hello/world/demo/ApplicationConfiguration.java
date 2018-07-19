88b8864776a6c68d06008e16c693dd95c4fc567c
rupendra <rupenrarajput7@gmail.com>
2018-07-19 14:24:36 +0530

    -hello_world_demo

--- hello_world_demo/src/main/java/com/hello/world/demo/ApplicationConfiguration.java
+++ hello_world_demo/src/main/java/com/hello/world/demo/ApplicationConfiguration.java
@@ -1,12 +1,14 @@
 package com.hello.world.demo;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class ApplicationConfiguration {
 	@RequestMapping("/hello")
-	public String hello() {
-		return "Hello World";
+	public String helloWithName(@RequestParam(name="name",required=false,defaultValue="world") String name) {
+		return "hello : "+name;
 	}
 }
